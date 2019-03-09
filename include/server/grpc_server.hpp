088d0e83d77df96bbc79c813f66563de7c247878
Simon Königsreiter <koenigsreiter.simon@student.htlwrn.ac.at>
2019-03-09 17:38:22 +0100

    Adds doxygen comments for classes `grpc_server` and `health_checker`

--- include/server/grpc_server.hpp
+++ include/server/grpc_server.hpp
@@ -8,11 +8,47 @@
 #include "monitor.hpp"
 #include "tcp_monitor.hpp"
 
+/** Handles incoming GRPC calls.
+ * 
+ * This is a class that handles incoming GRPC calls.
+ * The actual definition of the server is defined in 
+ * `src/messages.proto`.
+ * 
+ * The class derives from the auto generated class
+ * `messages::Monitor::Service` and handles the `start`
+ * method.
+ * 
+ * @see https://grpc.io/ GRPC Website
+ */
 class grpc_server final : public messages::Monitor::Service {
 public:
-    grpc::Status start(grpc::ServerContext* ctx, const messages::Config* cfg, messages::Reply* repl) override;
+    /** Starts the actual monitor based on @p cfg
+     * @param ctx ServerContext of GRPC
+     * @param cfg Protobuf configuration message
+     * @param repl Output message; Empty reply
+     * 
+     * Responsible for starting a `monitor`. Depending on the 
+     * type of the server the respective monitor will be started.
+     * 
+     * @returns GRPC internal datastructure; 
+     *          `Status::Ok` if the `monitor` has been 
+     *          started and detached.
+     */
+    grpc::Status start(grpc::ServerContext* ctx, 
+                       const messages::Config* cfg, 
+                       messages::Reply* repl) override;
+    /** Default destructor. 
+     */
     ~grpc_server() = default;
 private:
+    /** Utility method to start the thread and detach it
+     * @param inp The `monitor` class that is going to be started as a thread
+     * @param cfg Config message containing neccessary information for the monitor
+     * 
+     * Generic method that takes a `monitor` and starts it in a seperate thread.
+     * It also extracts neccessary information of the @p cfg parameter and 
+     * passes it to the thread.
+     */
     template <class T> 
     void start_monitoring_thread(T& inp, const messages::Config* cfg) {
         std::thread monitoring_thread{inp, cfg->callback_address(), cfg->callback_port(), cfg->config()};
--- include/server/health_checker.hpp
+++ include/server/health_checker.hpp
@@ -1,8 +1,25 @@
 #ifndef HEALTH_CHECK_HPP
 #define HEALTH_CHECK_HPP
 
+/** Thread that listens HealthCheck messages.
+ * 
+ * Seperate thread to check whether the wether the 
+ * master is available or not.
+ * Creates a TCP socket on the specified port and
+ * continously listens for protobuf messages.
+ * 
+ */
 class health_checker {
 public:
+    /** Operator to start the thread easily.
+     * @param port The port to listen on for incoming connections
+     * 
+     * Creates a TCP socket and infinetely listens for
+     * `messages::HealthCheck` messages. If 
+     * the server is online it will reply with a 
+     * `messages::HealthMessage` where it says it is
+     * up.
+     */
     void operator()(unsigned short int port);
 
 };

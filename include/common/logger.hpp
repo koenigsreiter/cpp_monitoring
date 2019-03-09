5205d217fd2115560c2bcf4679fb1448cb38ada7
Simon Königsreiter <koenigsreiter.simon@student.htlwrn.ac.at>
2019-03-09 15:58:06 +0100

    Adds `logger` and `networking` documentation
    Adds the doxygen comments both for `logger.hpp` and `networking.hpp`
    files.

--- include/common/logger.hpp
+++ include/common/logger.hpp
@@ -5,8 +5,19 @@
 #include <spdlog/sinks/stdout_color_sinks.h>
 
 extern std::string LOGGER_NAME;
+
+/** Simple logging Singleton.
+ * 
+ * This class contains a public static reference to 
+ * an `spdlog::logger`. 
+ */
 class logger {
 public:
+    /** Reference to the actual logger.
+     * 
+     * A thread safe logger. Uses the extern string `LOGGER_NAME`
+     * to define the name of the logger.
+     */
     inline static std::shared_ptr<spdlog::logger> log = spdlog::stderr_color_mt(LOGGER_NAME);
 };
 
--- include/common/networking.hpp
+++ include/common/networking.hpp
@@ -5,10 +5,24 @@
 #include <asio.hpp>
 #include <google/protobuf/message.h>
 
+/** Static class containing various networking functionality.
+ * 
+ * This class is responsible for handling protobuf transportation
+ * over the network.
+ */
 class networking
 {
 public:
 
+    /** Sends the specified protobug message over the socket to the remote host.
+     * @param sock The TCP socket to the remote host used for transport
+     * @param msg The message to serialize and send
+     * 
+     * Sends the length in bytes first and the actual message afterwards.
+     * if an error occurs it will be captured and logged.
+     * 
+     * @returns True if the message was successfully sent; False otherwise. 
+     */
     static inline bool send_protobuf(asio::ip::tcp::socket& sock, google::protobuf::Message& msg) {
         try {
             u_int64_t msg_size{msg.ByteSizeLong()};
@@ -27,6 +41,15 @@ public:
         return true;
     }
 
+    /** Receives a protobuf message from the socket.
+     * @param sock TCP socket to the remote host
+     * @param msg Message that is going to be serialized
+     * 
+     * Reads the message size first from the socket and then serializes the message from
+     * the socket.
+     * 
+     * @returns True if the message could successfully be serialized; False otherwise.
+     */
     static inline bool receive_protobuf(asio::ip::tcp::socket& sock, google::protobuf::Message& msg) {
         u_int64_t msg_size;
         sock.receive(asio::buffer(&msg_size, sizeof(msg_size)), 0);
